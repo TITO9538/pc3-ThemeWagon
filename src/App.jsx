49b05ce5339d7d1c7165e519363e457753da60ad
Thomas E. Monzon M. <102255857+TITO9538@users.noreply.github.com>
2025-07-16 01:58:23 -0300

    resolve problems v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Services from "./pages/Services";
 import Details from "./pages/Details";
 import Features from "./pages/Features";
 import Projects from "./pages/Projects";
-import Team from "./pages/team";
+import Team from "./pages/Team";
 import Contact from "./pages/Contact";
 import { Background } from "./components/Background";
 
